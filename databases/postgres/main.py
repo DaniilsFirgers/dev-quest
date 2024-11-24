17ab7e532c57065b63f1103a2226a8d529a80bca
Daniils Firgers <dfirger@gmail.com>
2024-11-24 20:41:12 +0200

    qwe

--- databases/postgres/main.py
+++ databases/postgres/main.py
@@ -1,5 +1,13 @@
-import psycopg
+# import psycopg
+
+import requests
 
 
 def main():
-    pass
+    img_data = requests.get(
+        'https://i.ss.com/gallery/7/1278/319301/63860018.th2.jpg').content
+    with open('image_name.jpg', 'wb') as handler:
+        handler.write(img_data)
+
+
+main()
