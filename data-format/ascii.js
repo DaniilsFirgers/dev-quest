a301d92a5d553c55d25ea8618e8c843ce1bb6a76
Daniils Firgers <dfirger@gmail.com>
2026-01-04 12:15:01 +0200

    feat: start ascii

---
+++ data-format/ascii.js
@@ -0,0 +1,20 @@
+// Encoding - is the process of converting data, information or signals from one format to another for efficient storage.
+// Decoding - is the process of translating received messages into codewords of a given code.
+
+// ASCII (American Standard Code for Onformation Interchange) - a character encoding standard
+// that assigns unique numbers (0-127) to represent text, like letters, numbers and symbols allowing
+// computers to process and exchange text data consistently.
+
+// Each ASCII character is represented by a 7-bit binary number (2^7 = 128 possible values).
+
+// ASCII was designed smartly:
+// - Letters are in order
+// - Numbers are in order
+// - Uppercase and lowercase differ by 1 bit
+
+// BUT it is limited:
+// - Only enough for English
+// - Not enough for accents, emojis, non-Latin scripts
+
+// Mental model:
+// Bits → Number → ASCII table → Character
